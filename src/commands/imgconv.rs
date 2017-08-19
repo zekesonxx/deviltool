e495f31854dcd5cd2a5a3505bba7b3b3ea773365
Zeke Sonxx <zeke@zekesonxx.com>
2017-08-19 15:24:30 -0400

    Actually allow exporting to a named file in imgconv

--- src/commands/imgconv.rs
+++ src/commands/imgconv.rs
@@ -17,21 +17,22 @@ pub fn execute(matches: &ArgMatches) {
     reader.read_to_end(&mut buf);
     match tex2::tex2_image_boundless(buf.as_ref()) {
         Done(unused, mut tex2image) => {
-            println!("parsed!");
+            let mut output_file = if matches.is_present("OUTFILE") {
+                PathBuf::from(matches.value_of("OUTFILE").unwrap())
+            } else {
+                let mut file = PathBuf::from(matches.value_of("FILE").unwrap());
+                file.set_extension("png");
+                file
+            };
 
             let mut img = ImageBuffer::new(tex2image.width, tex2image.height);
-            if matches.is_present("phantom") {
-                tex2image.phantom = true;
-            }
             img.copy_from(&tex2image, 0, 0);
 
-            let mut output_file = PathBuf::from(matches.value_of("FILE").unwrap());
-            output_file.set_extension("png");
+            let ref mut fout = File::create(output_file.clone()).unwrap();
 
-            let ref mut fout = File::create(output_file).unwrap();
-
-            // We must indicate the image’s color type and what format to save as
             let _ = image::ImageRgba8(img).save(fout, image::PNG);
+
+            println!("Converted image saved to {}", output_file.display());
         },
         Error(err) => {
             println!("error: {:?}", err);
